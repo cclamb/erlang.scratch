77c2104f5e348f29d9203a0ff84db684c2ece3e7
Chris Lamb <cclamb@cs.unm.edu>
2010-08-22 22:43:56 -0600

    my_bank added

--- my_bank.erl
+++ my_bank.erl
@@ -1,15 +1,15 @@
 -module(my_bank).
 -behaviour(gen_server).
 -export([start/0]).
--export([init/1. handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -compile(export_all).
 
 start() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
-stop() -> gen_server:call(?MODULE, stop).
+stop() 	-> gen_server:call(?MODULE, stop).
 
-new_account(Who) -> gen_server:call(?MODULE, {new, Who}).
-deposit(Who, Amount) -> gen_server:call(?MODULE, {add, Who, Amount}).
-withdraw(Who, Amount) -> gen_server:call(?MODULE, {remove, Who, Amount}).
+new_account(Who) 		-> gen_server:call(?MODULE, {new, Who}).
+deposit(Who, Amount) 	-> gen_server:call(?MODULE, {add, Who, Amount}).
+withdraw(Who, Amount) 	-> gen_server:call(?MODULE, {remove, Who, Amount}).
 
 init([]) -> {ok, ets:new(?MODULE, [])}.
 
@@ -29,3 +29,21 @@ handle_call({add, Who, X}, _From, Tab) ->
 			{thanks, Who, your_balance_is, NewBalance}
 	end,
 	{reply, Reply, Tab};
+handle_call({remove, Who, X}, _From, Tab) ->
+	Reply = case ets:lookup(Tab, Who) of
+		[] -> not_a_customer;
+		[{Who, Balance}] when X =< Balance ->
+			NewBalance = Balance - X,
+			ets:insert(Tab, {Who, NewBalance}),
+			{thanks, Who, your_balance_is, NewBalance, in_the_bank};
+		[{Who, Balance}] ->
+			{sorry, Who, you_only_have, Balance, in_the_bank}
+	end,
+	{reply, Reply, Tab};
+handle_call(stop, _From, Tab) ->
+	{stop, normal, stopped, Tab}.
+	
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, Extra) -> {ok, State, Extra}.
