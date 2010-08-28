78873053f5d6cca96cee7276f155f301c0f12ff0
Chris Lamb <cclamb@cs.unm.edu>
2010-08-27 21:37:56 -0600

    added rpc to remote handler module

--- remote_handler.erl
+++ remote_handler.erl
@@ -1,17 +1,13 @@
 -module(remote_handler).
 -behaviour(gen_event).
 -export([init/1, handle_event/2, terminate/2, code_change/3, handle_call/2, handle_info/2]).
--export([add_handler/2]).
 
 init(_Args) ->
     {ok, []}.
 
-add_handler(EventMan, Args) ->
-	io:format("*** registering remote handler *** ~p~n", [?MODULE]),
-	gen_event:add_handler(EventMan, ?MODULE, Args).
-
 handle_event(ErrorMsg, State) ->
     io:format("*** transmitting to control node *** ~p~n", [ErrorMsg]),
+	rpc:call(foo@s907454, command_server, call, [{event, example_payload}]),
     {ok, State}.
 
 terminate(_Args, _State) ->
